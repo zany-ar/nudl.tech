14bfefb440ebfae746dfd2f5e9e17d02cc1c6002
0xCN <0xnoodles@gmail.com>
2022-07-26 20:12:48 +0000

    Deploying to gh-pages from @ 0xCN/nudl.tech@190bde9b06a5923f6d5a747f48e1d408ea883cbe 🚀

---
+++ _next/static/UIuZbJWgE39vvfffQds7A/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/274-41d2aebbd383dded.js","static/chunks/pages/index-3aa9940e1f22131d.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

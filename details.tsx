fe55cca20bfd9aa79a7f4605e1b6c21f86ef1c9c
0xCN <zanyar@nudl.tech>
2024-10-09 16:22:24 -0500

    fix: remove discord link

--- details.tsx
+++ details.tsx
@@ -1,23 +1,36 @@
-import { Helper } from "./src/components/Helper"
-import NextLink from "next/link"
-import { Link } from "@chakra-ui/react"
-import { IoCodeOutline, IoCodeSlashOutline, IoColorWandOutline, IoEarthOutline, IoLayersOutline, IoLeafOutline, IoLogoFigma, IoLogoJavascript, IoLogoNodejs, IoLogoPython, IoLogoReact, IoServerOutline, IoTriangleOutline } from "react-icons/io5"
-
+import { Helper } from "./src/components/Helper";
+import NextLink from "next/link";
+import { Link } from "@chakra-ui/react";
+import {
+  IoCodeOutline,
+  IoCodeSlashOutline,
+  IoColorWandOutline,
+  IoEarthOutline,
+  IoLayersOutline,
+  IoLeafOutline,
+  IoLogoFigma,
+  IoLogoJavascript,
+  IoLogoNodejs,
+  IoLogoPython,
+  IoLogoReact,
+  IoServerOutline,
+  IoTriangleOutline,
+} from "react-icons/io5";
 
 // <head> tag info
 
 export const head = {
-  title: 'nudlツ portfolio',
+  title: "nudlツ portfolio",
   description: `Hello! My name is Zanyar, I build web applications, freelance, tutor programming, and live off eating instant ramen 🍜.`,
-  keywords: "0xCN, nudlツ, Zanyar Jamal, Freelancer, Web Developer, Cyber Security Enthusiast, Full Stack Developer",
+  keywords:
+    "0xCN, nudlツ, Zanyar Jamal, Freelancer, Web Developer, Cyber Security Enthusiast, Full Stack Developer",
   url: "https://nudl.tech",
   image: "https://nudl.tech/screenshot.png",
-  googleAnalytics: "G-HL0YCRR2PD"
-}
-
+  googleAnalytics: "G-HL0YCRR2PD",
+};
 
 // portfolio info
-// { 
+// {
 //   email,
 //   intro,
 //   social,
@@ -25,232 +38,247 @@ export const head = {
 //   recentTechnologies,
 //   projects,
 //   sideProjects
-// } 
+// }
 
-export const email = "zanyar@nudl.tech"
+export const email = "zanyar@nudl.tech";
 
 export const intro = {
-  name: 'Zanyar Jamal',
-  moto: 'Full Stack Engineer',
+  name: "Zanyar Jamal",
+  moto: "Full Stack Engineer",
   text: (
     <>
-      I'm a software engineer who likes to build digital products and services, alias {' '}
-      <NextLink href={'https://github.com/0xCN/'} passHref>
-        <Link fontWeight={'600'} textDecoration={'underline'}>nudlツ</Link>
-      </NextLink>.
-      If you want to partner up on a project or just want to talk:
+      I'm a software engineer who likes to build digital products and services,
+      alias{" "}
+      <NextLink href={"https://github.com/0xCN/"} passHref>
+        <Link fontWeight={"600"} textDecoration={"underline"}>
+          nudlツ
+        </Link>
+      </NextLink>
+      . If you want to partner up on a project or just want to talk:
     </>
-  )
-}
+  ),
+};
 
 export const social = {
-  github: 'https://github.com/0xCN/',
-  discord: 'https://discordapp.com/users/608619591677247522'
-}
+  github: "https://github.com/0xCN/",
+  // discord: "https://discordapp.com/users/608619591677247522",
+};
 
 export const aboutMe = (
   <>
-    Hello! My name is <Helper>Zanyar</Helper>, I build web applications, distributed systems, and live off eating instant ramen 🍜.
+    Hello! My name is <Helper>Zanyar</Helper>, I build web applications,
+    distributed systems, and live off eating instant ramen 🍜.
+    <br />
+    <br />I first started out as a <Helper>cyber-security enthusiast</Helper> —
+    and learned programming only to build automated tools to make hacking easier
+    and faster.
     <br />
     <br />
-    I first started out as a <Helper>cyber-security enthusiast</Helper> — and learned programming only to build automated tools to make hacking easier and faster.
-    <br />
-    <br />
-    Since then, I’ve updated my repertoire with the <Helper>latest technologies</Helper>, and have even become a jack of all trades and worked in most fields including {' '}
-    <Helper>System Design</Helper>, <Helper>Mobile App Development</Helper>, and my current occupation a <Helper>Full Stack Engineer</Helper>.
+    Since then, I’ve updated my repertoire with the{" "}
+    <Helper>latest technologies</Helper>, and have even become a jack of all
+    trades and worked in most fields including <Helper>System Design</Helper>,{" "}
+    <Helper>Mobile App Development</Helper>, and my current occupation a{" "}
+    <Helper>Full Stack Engineer</Helper>.
     <br />
     <br />
     Here are a few technologies I’ve been working with recently:
   </>
-)
+);
 
-export const recentTechnologies = ["Next.js", "TypeScript", "MongoDB", "Redis", "Digital Ocean", "Chakra-UI"]
+export const recentTechnologies = [
+  "Next.js",
+  "TypeScript",
+  "MongoDB",
+  "Redis",
+  "Digital Ocean",
+  "Chakra-UI",
+];
 
 export const projects = [
   {
-    title: 'Pentesting Jobs',
+    title: "Pentesting Jobs",
     description: `A job board for cyber-security experts, find / post jobs.`,
-    link: 'https://pentestingjobs.co',
+    link: "https://pentestingjobs.co",
     techstack: ["TypeScript", "Next.js", "MongoDB", "Material-UI"],
-    imgsrc: '/images/pentesting_jobs.png'
+    imgsrc: "/images/pentesting_jobs.png",
   },
   {
-    title: 'Nudolio',
+    title: "Nudolio",
     description: `Open source software license key manager built with MERN & redux with a web api to build on.`,
-    link: 'https://nudolio.herokuapp.com/',
-    github: 'https://github.com/0xCN/nudolio',
+    link: "https://nudolio.herokuapp.com/",
+    github: "https://github.com/0xCN/nudolio",
     techstack: ["Express.js", "React.js", "Redux", "MongoDB"],
-    imgsrc: '/images/nudolio.png'
-  }
-]
+    imgsrc: "/images/nudolio.png",
+  },
+];
 
 export const sideProjects = [
   {
-    title: 'Workaholic',
+    title: "Workaholic",
     description: `A time manager that shouts at you when you are not doing your work.`,
     techstack: ["Python", "JavaScript", "Chrome Extension"],
-    github: 'https://github.com/0xCN/workaholic'
+    github: "https://github.com/0xCN/workaholic",
   },
   {
-    title: 'Kawaii-Vault',
+    title: "Kawaii-Vault",
     description: `Some sort of password manager that uses GitHub's Gist service to store its encrypted data.`,
     techstack: ["Rust", "AES-Encryption", "GitHub API"],
-    github: 'https://github.com/0xCN/kawaii-vault'
+    github: "https://github.com/0xCN/kawaii-vault",
   },
   {
-    title: '0xVault',
+    title: "0xVault",
     description: `Flutter project, It's an app for taking notes & saving your passwords with GitHub Gist's API.`,
     techstack: ["Flutter", "Dart", "GitHub API"],
-    github: 'https://github.com/0xCN/0xVAULT',
-    youtube: 'https://www.youtube.com/watch?v=YMaDtI__OQE'
+    github: "https://github.com/0xCN/0xVAULT",
+    youtube: "https://www.youtube.com/watch?v=YMaDtI__OQE",
   },
   {
-    title: 'WeebMyIp',
+    title: "WeebMyIp",
     description: `Website with an API to get information on IP address and an anime girl to read your IP for you`,
-    techstack: ['Python', 'Flask', 'HTML5', 'CSS3'],
-    github: 'https://github.com/0xanteiku/weebmyip',
-    link: 'https://weebmyip.pythonanywhere.com'
+    techstack: ["Python", "Flask", "HTML5", "CSS3"],
+    github: "https://github.com/0xanteiku/weebmyip",
+    link: "https://weebmyip.pythonanywhere.com",
   },
   {
-    title: 'MailBot',
+    title: "MailBot",
     description: `A discord bot which forwards new emails to your discord server, you can reply or send new emails.`,
-    techstack: ['Python', 'AioIMAPLib', 'AioSMTPLib'],
-    github: 'https://github.com/0xCN/MailBot'
+    techstack: ["Python", "AioIMAPLib", "AioSMTPLib"],
+    github: "https://github.com/0xCN/MailBot",
   },
   {
-    title: 'PasteJS',
+    title: "PasteJS",
     description: `A chrome extension to prevent both CSS & JS PasteJacking attacks`,
-    techstack: ['JavaScript', 'HTML5'],
-    github: 'https://github.com/0xCN/PasteJS'
-  }
-]
+    techstack: ["JavaScript", "HTML5"],
+    github: "https://github.com/0xCN/PasteJS",
+  },
+];
 
-export const tabs = ["Web Development", "Web Design", "Database", "General"]
+export const tabs = ["Web Development", "Web Design", "Database", "General"];
 export const techstack = [
   {
-    name: 'Visual Studio Code',
-    image: '/tech/vscode.png',
-    tab: 'General',
-    icon: (<IoCodeSlashOutline fontSize={'34px'} />)
+    name: "Visual Studio Code",
+    image: "/tech/vscode.png",
+    tab: "General",
+    icon: <IoCodeSlashOutline fontSize={"34px"} />,
   },
   {
-    name: 'Next.js',
-    image: '/tech/nextjs.png',
-    tab: 'Web Development',
-    icon: (<IoTriangleOutline fontSize={'34px'} />)
+    name: "Next.js",
+    image: "/tech/nextjs.png",
+    tab: "Web Development",
+    icon: <IoTriangleOutline fontSize={"34px"} />,
   },
   {
-    name: 'Figma',
-    image: '/tech/figma.png',
-    tab: 'Web Design',
-    icon: (<IoLogoFigma fontSize={'34px'} />)
+    name: "Figma",
+    image: "/tech/figma.png",
+    tab: "Web Design",
+    icon: <IoLogoFigma fontSize={"34px"} />,
   },
   {
-    name: 'React.js',
-    image: '/tech/reactjs.png',
-    tab: 'Web Development',
-    icon: (<IoLogoReact fontSize={'34px'} />)
+    name: "React.js",
+    image: "/tech/reactjs.png",
+    tab: "Web Development",
+    icon: <IoLogoReact fontSize={"34px"} />,
   },
   {
-    name: 'Express.js',
-    image: '/tech/expressjs.png',
-    tab: 'Web Development',
-    icon: (<IoLogoNodejs fontSize={'34px'} />)
+    name: "Express.js",
+    image: "/tech/expressjs.png",
+    tab: "Web Development",
+    icon: <IoLogoNodejs fontSize={"34px"} />,
   },
   {
-    name: 'MongoDB',
-    image: '/tech/mongodb.png',
-    tab: 'Database',
-    icon: (<IoLeafOutline fontSize={'34px'} />)
+    name: "MongoDB",
+    image: "/tech/mongodb.png",
+    tab: "Database",
+    icon: <IoLeafOutline fontSize={"34px"} />,
   },
   {
-    name: 'JavaScript',
-    image: '/tech/javascript.png',
-    tab: 'General',
-    icon: (<IoLogoJavascript fontSize={'34px'} />)
+    name: "JavaScript",
+    image: "/tech/javascript.png",
+    tab: "General",
+    icon: <IoLogoJavascript fontSize={"34px"} />,
   },
   {
-    name: 'TypeScript',
-    image: '/tech/typescript.png',
-    tab: 'General',
-    icon: (<IoCodeOutline fontSize={'34px'} />)
+    name: "TypeScript",
+    image: "/tech/typescript.png",
+    tab: "General",
+    icon: <IoCodeOutline fontSize={"34px"} />,
   },
   {
-    name: 'PHP',
-    image: '/tech/php.png',
-    tab: 'General',
-    icon: (<IoCodeOutline fontSize={'34px'} />)
+    name: "PHP",
+    image: "/tech/php.png",
+    tab: "General",
+    icon: <IoCodeOutline fontSize={"34px"} />,
   },
   {
-    name: 'Python',
-    image: '/tech/python.png',
-    tab: 'General',
-    icon: (<IoLogoPython fontSize={'34px'} />)
+    name: "Python",
+    image: "/tech/python.png",
+    tab: "General",
+    icon: <IoLogoPython fontSize={"34px"} />,
   },
   {
-    name: 'Node.js',
-    image: '/tech/nodejs.png',
-    tab: 'General',
-    icon: (<IoLogoNodejs fontSize={'34px'} />)
+    name: "Node.js",
+    image: "/tech/nodejs.png",
+    tab: "General",
+    icon: <IoLogoNodejs fontSize={"34px"} />,
   },
   {
-    name: 'PostgreSQL',
-    image: '/tech/postgresql.png',
-    tab: 'Database',
-    icon: (<IoServerOutline fontSize={'34px'} />)
+    name: "PostgreSQL",
+    image: "/tech/postgresql.png",
+    tab: "Database",
+    icon: <IoServerOutline fontSize={"34px"} />,
   },
   {
-    name: 'MySQL',
-    image: '/tech/mysql.png',
-    tab: 'Database',
-    icon: (<IoServerOutline fontSize={'34px'} />)
+    name: "MySQL",
+    image: "/tech/mysql.png",
+    tab: "Database",
+    icon: <IoServerOutline fontSize={"34px"} />,
   },
   {
-    name: 'Rust Lang',
-    image: '/tech/rustlang.png',
-    tab: 'General',
-    icon: (<IoCodeOutline fontSize={'34px'} />)
+    name: "Rust Lang",
+    image: "/tech/rustlang.png",
+    tab: "General",
+    icon: <IoCodeOutline fontSize={"34px"} />,
   },
   {
-    name: 'CSharp',
-    image: '/tech/csharp.png',
-    tab: 'General',
-    icon: (<IoCodeOutline fontSize={'34px'} />)
+    name: "CSharp",
+    image: "/tech/csharp.png",
+    tab: "General",
+    icon: <IoCodeOutline fontSize={"34px"} />,
   },
   {
-    name: 'Redis',
-    image: '/tech/redis.png',
-    tab: 'Database',
-    icon: (<IoLayersOutline fontSize={'34px'} />)
+    name: "Redis",
+    image: "/tech/redis.png",
+    tab: "Database",
+    icon: <IoLayersOutline fontSize={"34px"} />,
   },
   {
-    name: 'Django',
-    image: '/tech/django.png',
-    tab: 'Web Development',
-    icon: (<IoEarthOutline fontSize={'34px'} />)
+    name: "Django",
+    image: "/tech/django.png",
+    tab: "Web Development",
+    icon: <IoEarthOutline fontSize={"34px"} />,
   },
   {
-    name: 'Flask',
-    image: '/tech/flask.png',
-    tab: 'Web Development',
-    icon: (<IoEarthOutline fontSize={'34px'} />)
+    name: "Flask",
+    image: "/tech/flask.png",
+    tab: "Web Development",
+    icon: <IoEarthOutline fontSize={"34px"} />,
   },
   {
-    name: '.Net Core',
-    image: '/tech/dotnetcore.png',
-    tab: 'Web Development',
-    icon: (<IoEarthOutline fontSize={'34px'} />)
+    name: ".Net Core",
+    image: "/tech/dotnetcore.png",
+    tab: "Web Development",
+    icon: <IoEarthOutline fontSize={"34px"} />,
   },
   {
-    name: 'Chakra UI',
-    image: '/tech/chakraui.png',
-    tab: 'Web Design',
-    icon: (<IoColorWandOutline fontSize={'34px'} />)
+    name: "Chakra UI",
+    image: "/tech/chakraui.png",
+    tab: "Web Design",
+    icon: <IoColorWandOutline fontSize={"34px"} />,
   },
   {
-    name: 'Material UI',
-    image: '/tech/materialui.png',
-    tab: 'Web Design',
-    icon: (<IoColorWandOutline fontSize={'34px'} />)
-  }
-]
+    name: "Material UI",
+    image: "/tech/materialui.png",
+    tab: "Web Design",
+    icon: <IoColorWandOutline fontSize={"34px"} />,
+  },
+];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,53 +11,78 @@ import {
   ListIcon,
   List,
   SimpleGrid,
-} from '@chakra-ui/react'
-import { CopyIcon, EmailIcon } from '@chakra-ui/icons'
-import { Container } from '../components/layout/Container'
-import { Main } from '../components/layout/Main'
-import { Footer } from '../components/layout/Footer'
-import Navbar from '../components/layout/Navbar'
-import { aboutMe, email, head, intro, projects, recentTechnologies, sideProjects, social, techstack } from '../../details'
-import { IoFileTrayFull, IoIdCard, IoLayers, IoPlay, IoTerminal } from 'react-icons/io5'
-import ProjectCard from '../components/ProjectCard'
-import { SideProjectCard } from '../components/SideProjectCard'
-import { Social } from '../components/Socail'
-import { TechStack } from '../components/TechStack'
-import Script from 'next/script'
-
+} from "@chakra-ui/react";
+import { CopyIcon, EmailIcon } from "@chakra-ui/icons";
+import { Container } from "../components/layout/Container";
+import { Main } from "../components/layout/Main";
+import { Footer } from "../components/layout/Footer";
+import Navbar from "../components/layout/Navbar";
+import {
+  aboutMe,
+  email,
+  head,
+  intro,
+  projects,
+  recentTechnologies,
+  sideProjects,
+  social,
+  techstack,
+} from "../../details";
+import {
+  IoFileTrayFull,
+  IoIdCard,
+  IoLayers,
+  IoPlay,
+  IoTerminal,
+} from "react-icons/io5";
+import ProjectCard from "../components/ProjectCard";
+import { SideProjectCard } from "../components/SideProjectCard";
+import { Social } from "../components/Socail";
+import { TechStack } from "../components/TechStack";
+import Script from "next/script";
 
 const Index = () => {
   return (
     <Container>
       {/* <!-- Google Analytics --> */}
-      <Script strategy='afterInteractive'>
+      <Script strategy="afterInteractive">
         {`
             window.ga = window.ga || function () { (ga.q = ga.q || []).push(arguments) };ga.l=+new Date;
             ga('create', '${head.googleAnalytics}', 'auto');
             ga('send', 'pageview');
             `}
       </Script>
-      <Script strategy='afterInteractive' async src='https://www.google-analytics.com/analytics.js'></Script>
+      <Script
+        strategy="afterInteractive"
+        async
+        src="https://www.google-analytics.com/analytics.js"
+      ></Script>
       {/* <!-- End Google Analytics --> */}
       <Navbar />
-      <Main id='intro'>
-
+      <Main id="intro">
         {/* Intro */}
-
+        <Text color={"gray"} fontFamily={"mono"}>
+          note: this portfolio is outdated
+        </Text>
         <Box>
-          <Text color={'helper'} fontFamily={'mono'}>
+          <Text color={"helper"} fontFamily={"mono"}>
             Hi, my name is
           </Text>
-          <Heading as='h1' size={'2xl'} mt='20px !important'>
+          <Heading as="h1" size={"2xl"} mt="20px !important">
             {intro.name}
           </Heading>
-          <Heading as='h1' color={'text'} fontSize={'33px'} mt='30px !important' >
+          <Heading
+            as="h1"
+            color={"text"}
+            fontSize={"33px"}
+            mt="30px !important"
+          >
             {intro.moto}
           </Heading>
-          <Text color="text" maxW={'600px'} mt='30px !important'>
+          <Text color="text" maxW={"600px"} mt="30px !important">
             {intro.text}
           </Text>
-          <Box mt={'55px !important'}>
+          <Box mt={"55px !important"}>
             <Button
               as={Link}
               isExternal
@@ -67,43 +92,60 @@ const Index = () => {
               rounded="button"
               width="190px"
               leftIcon={<EmailIcon />}
-              size={'lg'}
+              size={"lg"}
             >
               Get In Touch
             </Button>
-            <Tooltip label='Copy Email' placement='top'>
+            <Tooltip label="Copy Email" placement="top">
               <IconButton
-                variant={'solid'}
+                variant={"solid"}
                 width="40px"
                 aria-label="Copy Email"
                 onClick={() => navigator.clipboard.writeText(email)}
                 rounded="button"
-                colorScheme={useColorModeValue('accent', 'gray')}
-                ml="10px" >
+                colorScheme={useColorModeValue("accent", "gray")}
+                ml="10px"
+              >
                 <CopyIcon />
               </IconButton>
             </Tooltip>
           </Box>
-          <Box mt={'30px !important'}>
-            <Social
-              {...social} />
+          <Box mt={"30px !important"}>
+            <Social {...social} />
           </Box>
         </Box>
 
         {/* About Me */}
 
-        <Heading id={'about-me'} top="40px" position='relative' visibility={'hidden'} />
-        <Box textAlign={'left'} mt={'150px !important'}>
-          <Heading as='h1' size={'md'} alignItems='center' display="inline-flex">
-            <Text color={"helper"} mr='10px'><IoIdCard /></Text> About Me
+        <Heading
+          id={"about-me"}
+          top="40px"
+          position="relative"
+          visibility={"hidden"}
+        />
+        <Box textAlign={"left"} mt={"150px !important"}>
+          <Heading
+            as="h1"
+            size={"md"}
+            alignItems="center"
+            display="inline-flex"
+          >
+            <Text color={"helper"} mr="10px">
+              <IoIdCard />
+            </Text>{" "}
+            About Me
           </Heading>
           <br />
           <br />
-          <Text color="text" maxW={'600px'}>
+          <Text color="text" maxW={"600px"}>
             {aboutMe}
           </Text>
-          <Box mt={'30px'}>
-            <List display={'grid'} gap={'5px 10px'} gridTemplateColumns={'repeat(2, minmax(100px, 200px))'} >
+          <Box mt={"30px"}>
+            <List
+              display={"grid"}
+              gap={"5px 10px"}
+              gridTemplateColumns={"repeat(2, minmax(100px, 200px))"}
+            >
               {recentTechnologies.map((tech, i) => (
                 <ListItem key={i}>
                   <ListIcon as={IoPlay} color="helper" />
@@ -116,15 +158,26 @@ const Index = () => {
 
         {/* Projects */}
 
-        <Heading id={'projects'} position='relative' top={'40px'} visibility={'hidden'} />
-        <Box textAlign={'center'} mt={'150px !important'}>
-          <Heading as='h1' size={'md'} alignItems='center' display="inline-flex">
-            <Text color={"helper"} mr='10px'><IoTerminal /></Text> Projects
+        <Heading
+          id={"projects"}
+          position="relative"
+          top={"40px"}
+          visibility={"hidden"}
+        />
+        <Box textAlign={"center"} mt={"150px !important"}>
+          <Heading
+            as="h1"
+            size={"md"}
+            alignItems="center"
+            display="inline-flex"
+          >
+            <Text color={"helper"} mr="10px">
+              <IoTerminal />
+            </Text>{" "}
+            Projects
           </Heading>
           <br />
-          <Text color={'text'}>
-            Some of my public projects.
-          </Text>
+          <Text color={"text"}>Some of my public projects.</Text>
           <br />
           <Box>
             {projects.map((project, i) => (
@@ -133,42 +186,45 @@ const Index = () => {
                 right={i % 2 == 0 ? true : false}
                 title={project.title}
                 description={project.description}
-                fontFamily={'gugi'}
+                fontFamily={"gugi"}
                 fontWeight={200}
                 techstack={project.techstack}
                 imgsrc={project.imgsrc}
-                link={project['link']}
-                github={project['github']} />
+                link={project["link"]}
+                github={project["github"]}
+              />
             ))}
-
           </Box>
         </Box>
 
         {/* Side Projects */}
 
-        <Box textAlign={'center'} mt={'150px !important'}>
-          <Heading as='h1' size={'md'} alignItems='center' display="inline-flex">
-            <Text color={"helper"} mr='10px'><IoFileTrayFull /></Text> Side Projects
+        <Box textAlign={"center"} mt={"150px !important"}>
+          <Heading
+            as="h1"
+            size={"md"}
+            alignItems="center"
+            display="inline-flex"
+          >
+            <Text color={"helper"} mr="10px">
+              <IoFileTrayFull />
+            </Text>{" "}
+            Side Projects
           </Heading>
           <br />
-          <Text color={'text'}>
-            Projects which made me smile!
-          </Text>
+          <Text color={"text"}>Projects which made me smile!</Text>
           <br />
           <br />
-          <SimpleGrid
-            columns={{ base: 1, md: 2 }}
-            spacing='20px'
-          >
+          <SimpleGrid columns={{ base: 1, md: 2 }} spacing="20px">
             {sideProjects.map((data, i) => (
               <SideProjectCard
                 key={i}
                 title={data.title}
                 description={data.description}
                 techstack={data.techstack}
-                github={data['github']}
-                link={data['link']}
-                youtube={data['youtube']}
+                github={data["github"]}
+                link={data["link"]}
+                youtube={data["youtube"]}
               />
             ))}
           </SimpleGrid>
@@ -176,15 +232,26 @@ const Index = () => {
 
         {/* Tech Stack */}
 
-        <Heading id={'tech-stack'} position='relative' top={'40px'} visibility={'hidden'} />
-        <Box textAlign={'center'} mt={'150px !important'}>
-          <Heading as='h1' size={'md'} alignItems='center' display="inline-flex">
-            <Text color={"helper"} mr='10px'><IoLayers /></Text> Tech Stack
+        <Heading
+          id={"tech-stack"}
+          position="relative"
+          top={"40px"}
+          visibility={"hidden"}
+        />
+        <Box textAlign={"center"} mt={"150px !important"}>
+          <Heading
+            as="h1"
+            size={"md"}
+            alignItems="center"
+            display="inline-flex"
+          >
+            <Text color={"helper"} mr="10px">
+              <IoLayers />
+            </Text>{" "}
+            Tech Stack
           </Heading>
           <br />
-          <Text color={'text'}>
-            Technologies I use often.
-          </Text>
+          <Text color={"text"}>Technologies I use often.</Text>
           <br />
           <TechStack techstack={techstack} />
         </Box>
@@ -192,8 +259,7 @@ const Index = () => {
         <Footer />
       </Main>
     </Container>
-  )
-}
+  );
+};
 
-
-export default Index
+export default Index;
